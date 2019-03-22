331266fa965fe3b61136fc18a7f5c2a77daf6dca
hrnoh <hr_noh@korea.ac.kr>
2019-03-22 20:49:03 +0900

    add comments

--- Assignment1/part2/meanSEM.m
+++ Assignment1/part2/meanSEM.m
@@ -1,14 +1,18 @@
-% meanSEM: calculate mean and standard error of the mean(SEM) from the data
+% meanSEM: calculate mean and Standard Error of the Mean(SEM) from multiple
+% datas
 %   Input variables
-%       data : datas
+%       data : datas (2-D matrix)
 %   Output variables
-%       means : mean of given datas
-%       SEM : standard error of the mean of given datas
+%       means : mean of given datas (1-D vector)
+%       SEM : Standard Error of the Mean for given datas (1-D vector)
+%
+%   created : 2019/03/22
+%   modified : 2019/03/22
 
 function [means, SEMs] = meanSEM(data)
-% calculate means of datas
+% calculate means for given datas
 means = mean(data);
 
-% calculate SEM of datas
+% calculate SEM for given datas
 stds = std(data);
 SEMs = stds ./ sqrt(size(data, 1) - 1);
