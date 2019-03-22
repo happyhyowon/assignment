d111dd7af777876ad24eb5908f8fd420a4f1a903
hrnoh <hr_noh@korea.ac.kr>
2019-03-23 00:26:34 +0900

    add comments

--- Assignment1/part2/processData.m
+++ Assignment1/part2/processData.m
@@ -1,35 +1,77 @@
-% preamble
+% processData: This is a script that analyzing some example data
+
+%   Input variables
+%       X
+%   Output variables
+%       analysis.fig : Graphs for each variable including means and SEMs 
+%       analysis2.fig : Scatter plots for relationships between variables
+%
+%   created : 2019/03/22
+%   modified : 2019/03/22
+
+% Initialization
 clc; clear all; close all;
 
+% Load sample data
 load('experimentData');
+
+% convert table to array
 datas = table2array(experimentData);
 
-participant = 1:size(datas, 1);
-
+% calculate means and SEMs for heart rate, weight and exercise
 [means SEMs] = meanSEM(datas);
 
+% draw graphs for each variable including means and SEMs
+% create new window
 figure(1);
+
+% helper variable for drawing plots
 ylabels = ["Heart rate", "Weight", "Exercise"];
+% draw plots
 for i=1:3
+    % set i-th graph
     subplot(3, 1, i); hold on;
+    
+    % create x, y labels
     xlabel('participants');
     ylabel(ylabels(i));
+    
+    % plot red line for i-th datas 
     plot(datas(:, i), 'r');
+    
+    % plot mean for i-th variable 
     yline(means(i));
+    
+    % plot SEM for i-th variable
     yline(means(i) + SEMs(i), 'b--');
     yline(means(i) - SEMs(i), 'b--');
 end
+
+% save plots
 saveas(gcf, 'analysis.fig');
 
+% draw scatter plots for relationships between variables
+% create new window
 figure(2);
+
+% helper variables for drawing scatter plots
 dataIndecies = [1 2; 1 3; 2 3]
 ylabels = [["Heart rate", "Weight"],
            ["Heart rate", "Exercise"],
            ["Weight", "Exercise"]];
+       
+% draw scatter plots
 for i=1:3
+    % set i-th graph
     subplot(3, 1, i); hold on;
+    
+    % create x, y labels
     xlabel(ylabels(i, 1));
     ylabel(ylabels(i, 2));
+    
+    % plot points for relationships between variables
     scatter(datas(:, dataIndecies(i,1)), datas(:, dataIndecies(i,2)));
 end
+
+% save scatter plots
 saveas(gcf, 'analysis2.fig');
