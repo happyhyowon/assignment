38f711fc574e3cc8fde461b6459cea02e87d90e4
hrnoh <hr_noh@korea.ac.kr>
2019-03-24 22:09:16 +0900

    revision2

--- Assignment1/part1/yyyymmdd2secs.m
+++ Assignment1/part1/yyyymmdd2secs.m
@@ -8,12 +8,17 @@
 % created  : 2019/03/18
 % modified : 2019/03/23
 
+% 3 -> 1, 2월을 초로
+% 5 -> 1, 2, 3, 4월을 초로
+% sum(guulMonth(1:currentMonth-1) .* DayToSec)
+
 function secOfLife = yyyymmdd2secs(birth)
 guulMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+MintoSec = 60;
 HourtoSec = 60 * 60;
 DayToSec = 24 * HourtoSec;
-MonthToSec = 30 * DayToSec;
-YearToSec = MonthToSec * 365;
+MonthToSec = guulMonth .* DayToSec;
+YearToSec = sum(MonthToSec);
 
 % initial value
 secOfLife = NaN;
@@ -22,7 +27,7 @@ secOfLife = NaN;
 bYear = (uint64(birth(1) - '0') * 1000) + (uint64(birth(2) - '0') * 100) + (uint64(birth(3) - '0') * 10) + uint64(birth(4) - '0');
 bMonth = (uint64(birth(5) - '0') * 10) + uint64(birth(6) - '0');
 bDay = (uint64(birth(7) - '0') * 10) + uint64(birth(8) - '0');
-bTime = 24; % 12 AM (fixed)
+bTime = 0; % 12 AM (fixed)
 
 % to change current time type to uint64
 currentDay = uint64(clock);
@@ -30,13 +35,33 @@ cYear = uint64(currentDay(1));
 cMonth = uint64(currentDay(2));
 cDay = uint64(currentDay(3));
 cTime = uint64(currentDay(4));
+cMin = uint64(currentDay(5));
+cSec = uint64(currentDay(6));
+
+% Exception for Invalid input
+if bYear > cYear || (bMonth < 1 || bMonth > 12) || (bDay > guulMonth(bMonth))
+    error("Invalid input!!");
+end
 
 % calculate time expressed in sec
-myLifeSec = uint64((bYear *  YearToSec) + (bYear * MonthToSec) + (bDay * DayToSec) + (bTime * HourtoSec));
-curSec = uint64((cYear *  YearToSec) + (cMonth * MonthToSec) + (cDay * DayToSec) + (cTime * HourtoSec));
+if cMonth == 1
+   cMonth = 0;
+else
+   cMonth = (cMonth-1) * sum(guulMonth(1:cMonth-1) .* DayToSec);
+end
+
+if bMonth == 1
+   bMonth = 0;
+else
+   bMonth = (bMonth-1) * sum(guulMonth(1:bMonth-1) .* DayToSec);
+end
+
+
+myLifeSec = uint64(((bYear-1) * YearToSec) + bMonth + ((bDay-1) * DayToSec));
+curSec = uint64(((cYear-1) *  YearToSec) + cMonth + ((cDay-1) * DayToSec) + (cTime * HourtoSec) + (cMin * MintoSec) + cSec);
 
 % abnormal case : error print
-if(myLifeSec < curSec)
+if(myLifeSec > curSec)
     error('There is invalid birth day!!!')
     return
 end
