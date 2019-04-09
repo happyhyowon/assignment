0d3a61dcf6ec35795007256ec5b3cd36d163c016
hrnoh <hr_noh@korea.ac.kr>
2019-04-09 16:08:16 +0900

    complete part2

--- Assignment2/part2/deriveImage.m
+++ Assignment2/part2/deriveImage.m
@@ -1,4 +1,4 @@
-% testDerivative : This is a script for derive images
+% testDerivative : This is a script for image derivative
 %   Input variables
 %       None
 %   Output variables
@@ -11,48 +11,72 @@
 close all;
 clear all; clc;
 
+% simple function for generating square image
 imf = @(x, y) (x >= 30 & x <= 70) & (y >= 30 & y <= 70);
+
+% generate 150 x 150 meshgrid
 [x, y] = meshgrid(1:150, 1:150);
+
+% generate 41 x 41 square
 img = double(imf(x, y));
 
+% show original square image
 figure('Name', 'original square');
 imagesc(img);
 
+% get size of original square image
 [row, column] = size(img);
+% calculate x-derivative
 for i=1:row
     dx(i, :) = diff(img(i, :));
 end
 
+% calculate y-derivative
 for i=1:column
     dy(:, i) = diff(img(:, i));
 end
 
+% show x-derivative image
 figure('Name', 'dx of square');
 imagesc(dx);
 
+% show y-derivative image
 figure('Name', 'dy of square');
 imagesc(dy);
 
-%% another image derivative
-clc; clear all; close all;
-lena = imread('lena.png');
-[row, column, channel] = size(lena);
+%% another image derivative (optional)
+% Initialization
+clc; clear all;
+close all;
 
+% read image of sana who is korean famous singer
+sana = imread('sana.jpg');
+[row, column, channel] = size(sana);
+
+% show original sana image
 figure('Name', 'original image');
-imagesc(lena);
+imagesc(sana);
+axis image; % for maintaining aspect ratio
 
+% index for channel-wise derivative
 for i=1:channel
+    % index for row-wise derivative
     for j=1:row
-        dx(j, :, channel) = diff(lena(j, :, channel));
+        dx(j, :, i) = diff(sana(j, :, i));
     end
 
+    % index for column-wise derivative
     for j=1:column
-        dy(:, j, channel) = diff(lena(:, j, channel));
+        dy(:, j, i) = diff(sana(:, j, i));
     end
 end
 
-figure('Name', 'dx');
-imagesc(dx);
+% show x-derivative image
+figure('Name', 'dx of sana image');
+imagesc(255- dx);
+axis image; % for maintaining aspect ratio
 
-figure('Name', 'dy');
-imagesc(dy);
+% show y-derivative image
+figure('Name', 'dy of sana image');
+imagesc(255 - dy);
+axis image; % for maintaining aspect ratio
