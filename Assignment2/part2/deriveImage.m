36273ef2b350325ec6ffb1e4328c52cc98bc587c
hrnoh <hr_noh@korea.ac.kr>
2019-04-09 11:28:23 +0900

    complete part2

--- Assignment2/part2/deriveImage.m
+++ Assignment2/part2/deriveImage.m
@@ -6,6 +6,7 @@
 %
 %   created : 2019/04/07
 %   modified : 2019/04/09
+
 % Initialization
 close all;
 clear all; clc;
@@ -14,39 +15,44 @@ imf = @(x, y) (x >= 30 & x <= 70) & (y >= 30 & y <= 70);
 [x, y] = meshgrid(1:150, 1:150);
 img = double(imf(x, y));
 
-figure(1);
+figure('Name', 'original square');
 imagesc(img);
 
 [row, column] = size(img);
 for i=1:row
-    dx(row, :) = diff(img(row, :));
+    dx(i, :) = diff(img(i, :));
 end
 
 for i=1:column
-    dy(:, column) = diff(img(:, column));
+    dy(:, i) = diff(img(:, i));
 end
 
-figure(2);
+figure('Name', 'dx of square');
 imagesc(dx);
 
-figure(3);
-%imagesc(dy);
+figure('Name', 'dy of square');
+imagesc(dy);
 
 %% another image derivative
 clc; clear all; close all;
 lena = imread('lena.png');
-[row, column] = size(lena);
+[row, column, channel] = size(lena);
 
-for i=1:row
-    dx(row, :) = diff(lena(row, :));
+figure('Name', 'original image');
+imagesc(lena);
+
+for i=1:channel
+    for j=1:row
+        dx(j, :, channel) = diff(lena(j, :, channel));
+    end
+
+    for j=1:column
+        dy(:, j, channel) = diff(lena(:, j, channel));
+    end
 end
 
-for i=1:column
-    dy(:, column) = diff(lena(:, column));
-end
+figure('Name', 'dx');
+imagesc(dx);
 
-figure(1);
-imshow(dx);
-
-figure(2);
-imshow(dy);
+figure('Name', 'dy');
+imagesc(dy);
